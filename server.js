ccd40e61e5b144d6a062e09c882a8bef5dd4d946
opiniakhir-collab <opiniakhir@gmail.com>
2026-09-01 18:16:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import RunwayML from "@runwayml/sdk";
 import { env } from "cloudflare:workers";
 import { httpServerHandler } from "cloudflare:node";
@@ -12,7 +11,6 @@ const app = express();
  * ============================
  */
 
-app.use(cors());
 
 app.use(
   express.json({
